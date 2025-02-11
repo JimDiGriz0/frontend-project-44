b9511f0530bec3d817db0cb7fc72af002acde442
digriz <vakhitov.ms28@gmail.com>
2025-02-11 13:13:37 +0300

    modified:   src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,7 +1,7 @@
 import { getRandomNumber, isEven } from '../utils.js';
 import runGame from '../index.js';
 
-const description = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const getEvenRoundData = () => {
   const question = getRandomNumber(1, 1000);
